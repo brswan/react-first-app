369c433a474ef3f32a5e69165611c727a2abef9d
Bryan Swan <bryan.swan@navy.mil>
2020-01-20 17:59:44 -0500

    custom controls, routing, toast

---
+++ src/components/CourseForm.js
@@ -0,0 +1,70 @@
+import React from "react";
+import TextInput from "./common/TextInput";
+// import SelectInput from "./common/SelectInput";
+
+function CourseForm(props) {
+  // const options = [
+  //   {
+  //     value: 0,
+  //     name: ""
+  //   },
+  //   {
+  //     value: 1,
+  //     name: "Cory House"
+  //   },
+  //   {
+  //     value: 2,
+  //     name: "Scott Allen"
+  //   }
+  // ];
+
+  return (
+    <form onSubmit={props.onSubmit}>
+      <TextInput
+        id="title"
+        name="title"
+        label="Title"
+        onChange={props.onChange}
+        value={props.course.title}
+      />
+
+      {/* <SelectInput
+        id="author"
+        name="author"
+        label="Author"
+        onChange={props.onChange}
+        value={props.course.authorId}
+        options={options}
+      /> */}
+
+      <div className="form-group">
+        <label htmlFor="author">Author</label>
+        <div className="field">
+          <select
+            id="author"
+            name="authorId"
+            onChange={props.onChange}
+            value={props.course.authorId || ""}
+            className="form-control"
+          >
+            <option value="" />
+            <option value="1">Cory House</option>
+            <option value="2">Scott Allen</option>
+          </select>
+        </div>
+      </div>
+
+      <TextInput
+        id="category"
+        name="category"
+        label="Category"
+        onChange={props.onChange}
+        value={props.course.category}
+      />
+
+      <input type="submit" value="Save" className="btn btn-primary" />
+    </form>
+  );
+}
+
+export default CourseForm;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import CoursesForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = props => {
-  debugger;
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      <CoursesForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
