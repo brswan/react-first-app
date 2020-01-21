f02eb3c2b09c4cbbbe63529465255a862807341c
Bryan Swan <bryan.swan@navy.mil>
2020-01-21 10:44:10 -0500

    Updates

---
+++ precache-manifest.e119cbf4a3cfece90559cd95f02125d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdc541a1a98f5deb8fcacda6d937291",
+    "url": "/brswan/react-first-app/index.html"
+  },
+  {
+    "revision": "1bd4e2568bdd003af4bf",
+    "url": "/brswan/react-first-app/static/css/2.5a4baa08.chunk.css"
+  },
+  {
+    "revision": "1bd4e2568bdd003af4bf",
+    "url": "/brswan/react-first-app/static/js/2.0011b201.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/brswan/react-first-app/static/js/2.0011b201.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fc534faf499553cc5d7",
+    "url": "/brswan/react-first-app/static/js/main.88b3e471.chunk.js"
+  },
+  {
+    "revision": "14b0360b130536bbfb3f",
+    "url": "/brswan/react-first-app/static/js/runtime-main.7e23e02a.js"
+  }
+]);
